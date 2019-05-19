3eb0b18c8b8b46737bddbef941cf0bbf1ced5489
manas <manas_vijaywargiya@oyespace.com>
2019-05-19 11:52:36 +0530

    First and last name's first letter is showing instead of image for now

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,10 @@ const Header = (props) =>  {
             <View style={[styles.viewStyle,{flexDirection:'row'}]}>
                 <View style={{flex:0.3,flexDirection:'row', justifyContent:'center',alignItems:'center', marginLeft:16}}>
                     <TouchableOpacity onPress={() => props.navigate.navigate('EditProfileScreen')}>
-                        <Image style={{width:30,height:30, borderRadius:15, borderColor:'orange',borderWidth:1,}} source={{ uri: global.viewImageURL + 'PERSON' + global.MyAccountID + '.jpg' }}/>
+                        <View style={{width:30,height:30, borderRadius:15, borderColor:'orange',borderWidth:1, justifyContent:'center',alignItems:'center'}}>
+                            <Text>{global.MyFirstName[0].toUpperCase()}{global.MyLastName[0].toUpperCase()}</Text>
+                        </View>
+                        {/* <Image style={{width:30,height:30, borderRadius:15, borderColor:'orange',borderWidth:1,}} source={{ uri: global.viewImageURL + 'PERSON' + global.MyAccountID + '.jpg' }}/> */}
                     </TouchableOpacity>
                     <Text style={{marginLeft:5}}>{global.MyFirstName}</Text>
                 </View>
