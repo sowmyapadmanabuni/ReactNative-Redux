b2dff6dd07fdd3b4d05f3c697079f5851cad5aca
manas <manas_vijaywargiya@oyespace.com>
2019-06-28 13:39:09 +0530

    Mobilevalid, header, OtpVerification, App added

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
-import React, {Component} from "react";
-import {ActivityIndicator, Image, Text, View} from "react-native";
-import {connect} from "react-redux";
-import {createAppContainer, createStackNavigator, createSwitchNavigator} from "react-navigation";
-import {mystyles} from "./pages/styles";
+import React, { Component, Fragment } from "react";
+import {
+  Platform,
+  TextInput,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity,
+  Alert,
+  Dimensions,
+  SafeAreaView
+} from "react-native";
+import { connect } from "react-redux";
+import SplashScreen from "./splash_screen_pages/SplashScreen";
+import {
+  createStackNavigator,
+  createSwitchNavigator,
+  createAppContainer
+} from "react-navigation";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
+
+import { mystyles } from "./pages/styles";
 import RegisterUser from "./pages/RegisterUser";
 import UpdateUser from "./pages/UpdateUser";
 import ViewUser from "./pages/ViewUser";
@@ -22,289 +44,297 @@ import CreateOrJoin from "./assocition_pages/CreateOrJoin";
 import addmembers from "./assocition_pages/addmembers";
 import CreateUnitsPotrait from "./assocition_pages/CreateUnitsPotrait";
 import store from "./src/store";
-import {persistStore} from "redux-persist";
-// import SchedulePatrolling from "./src/screens/Patrolling/SchedulePatrolling";
-// import PatrollingCommonHeader from "./src/components/NavigationalHeaders/PatrollingCommonHeader";
-// import PatrollingCheckPoints from "./src/screens/Patrolling/PatrollingCheckPoints";
-// import AddCheckPoints from "./src/screens/Patrolling/AddCheckPoints";
+import { persistStore } from "redux-persist";
 
+// import Spinner from "react-native-spinkit";
 
 const AuthStack = createStackNavigator(
-    {
-        MobileValid: {
-            screen: MobileValid,
-            navigationOptions: {
-                title: "Sign In",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#000000"
-            }
-        },
-
-        RegistrationPageScreen: {
-            screen: RegistrationPage,
-            navigationOptions: {
-                title: "Registration ",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#000000"
-            }
-        },
-
-        SelectMyRoleScreen: {
-            screen: SelectMyRole,
-            navigationOptions: {
-                title: "Syncing ",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#000000"
-            }
-        },
-
-        OTPVerificationScreen: {
-            screen: OTPVerification,
-            navigationOptions: {
-                title: "OTP Verification",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        }
+  {
+    MobileValid: {
+      screen: MobileValid,
+      navigationOptions: {
+        title: "Sign In",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#000000"
+      }
     },
-    {
-        headerMode: "none"
+
+    RegistrationPageScreen: {
+      screen: RegistrationPage,
+      navigationOptions: {
+        title: "Registration ",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#000000"
+      }
+    },
+
+    SelectMyRoleScreen: {
+      screen: SelectMyRole,
+      navigationOptions: {
+        title: "Syncing ",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#000000"
+      }
+    },
+
+    OTPVerificationScreen: {
+      screen: OTPVerification,
+      navigationOptions: {
+        title: "OTP Verification",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
     }
+  },
+  {
+    headerMode: "none"
+  }
 );
 
 const DashStack = createStackNavigator(
-    {
-        // SplashScreen: {
-        //   screen: SplashScreen,
-        //   navigationOptions: {
-        //     title: "OYE SAFE",
-        //     headerStyle: { backgroundColor: "#f05555" },
-        //     headerTintColor: "#ffffff"
-        //   }
-        // },
+  {
+    
 
-        AssnListScreen: {
-            screen: associationlist,
-            navigationOptions: {
-                title: "Association List",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        CreateAssnScreen: {
-            screen: CreateAssociation,
-            navigationOptions: {
-                title: "Create Association",
-                header: null,
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        Unit: {
-            screen: unitlist,
-            navigationOptions: {
-                title: "Units List",
-                headerStyle: {backgroundColor: "#FA9917"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        CreateUnitsScreen: {
-            screen: CreateUnitsPotrait, //CreateUnits,
-            navigationOptions: {
-                title: "Create Units",
-                headerStyle: {backgroundColor: "#FA9917"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        // SelectMyRoleScreen: {
-        //   screen: SelectMyRole,
-        //   navigationOptions: {
-        //     title: "Syncing ",
-        //     headerStyle: { backgroundColor: "#f05555" },
-        //     headerTintColor: "#000000"
-        //   }
-        // },
-
-        View: {
-            screen: ViewUser,
-            navigationOptions: {
-                title: "View User",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        ViewAll: {
-            screen: ViewAllUser,
-            navigationOptions: {
-                title: "View All User",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        Update: {
-            screen: UpdateUser,
-            navigationOptions: {
-                title: "Update User",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        Register: {
-            screen: RegisterUser,
-            navigationOptions: {
-                title: "Register User",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        Delete: {
-            screen: DeleteUser,
-            navigationOptions: {
-                title: "Delete User",
-                headerStyle: {backgroundColor: "#f05555"},
-                headerTintColor: "#ffffff"
-            }
-        },
-
-        ResDashBoard: {
-            screen: ResApp,
-        },
-
-        CreateOrJoinScreen: {
-            screen: CreateOrJoin,
-            navigationOptions: {
-                title: "Association"
-            }
-        },
-
-        addmembersScreen: {
-            screen: addmembers,
-            navigationOptions: {
-                title: "Association"
-            }
-        },
-        // schedulePatrolling: {
-        //     screen: SchedulePatrolling,
-        //     navigationOptions: {
-        //         title: "Schedule Patrolling",
-        //         header: props => <PatrollingCommonHeader isReportVisible={true} isHidden={false} {...props}/>,
-        //     }
-        // },
-        // patrollingCheckPoint: {
-        //     screen: PatrollingCheckPoints,
-        //     navigationOptions: {
-        //         title: "Patrolling Check Points",
-        //         header: props => <PatrollingCommonHeader isReportVisible={false} isHidden={false} {...props}/>
-        //     }
-        // },
-        // addCheckPoint: {
-        //     screen: AddCheckPoints,
-        //     navigationOptions: {
-        //         title: "Add Checkpoints",
-        //         header: props => <PatrollingCommonHeader isReportVisible={false} isHidden={true} {...props}/>
-        //     }
-        // }
+    AssnListScreen: {
+      screen: associationlist,
+      navigationOptions: {
+        title: "Association List",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
     },
-    {
-        headerMode: "none",
-        initialRouteName: "ResDashBoard"
+
+    CreateAssnScreen: {
+      screen: CreateAssociation,
+      navigationOptions: {
+        title: "Create Association",
+        header: null,
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    Unit: {
+      screen: unitlist,
+      navigationOptions: {
+        title: "Units List",
+        headerStyle: { backgroundColor: "#FA9917" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    CreateUnitsScreen: {
+      screen: CreateUnitsPotrait, //CreateUnits,
+      navigationOptions: {
+        title: "Create Units",
+        headerStyle: { backgroundColor: "#FA9917" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    View: {
+      screen: ViewUser,
+      navigationOptions: {
+        title: "View User",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    ViewAll: {
+      screen: ViewAllUser,
+      navigationOptions: {
+        title: "View All User",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    Update: {
+      screen: UpdateUser,
+      navigationOptions: {
+        title: "Update User",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    Register: {
+      screen: RegisterUser,
+      navigationOptions: {
+        title: "Register User",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    Delete: {
+      screen: DeleteUser,
+      navigationOptions: {
+        title: "Delete User",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+      }
+    },
+
+    ResDashBoard: {
+      screen: ResApp,
+      navigationOptions: {
+        title: "ResidentDashBoard Drawer ",
+        headerStyle: { backgroundColor: "#f05555" },
+        headerTintColor: "#ffffff"
+        // header:null
+      }
+    },
+
+    CreateOrJoinScreen: {
+      screen: CreateOrJoin,
+      navigationOptions: {
+        title: "Association"
+      }
+    },
+
+    addmembersScreen: {
+      screen: addmembers,
+      navigationOptions: {
+        title: "Association"
+      }
     }
+  },
+  {
+    headerMode: "none",
+    initialRouteName: "ResDashBoard"
+  }
 );
 
 class Loading extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-    constructor(props) {
-        super(props);
-    }
+  componentDidMount() {
+    persistStore(store, null, () => {
+      const { signedIn } = this.props;
+      this.props.navigation.navigate(signedIn ? "App" : "Auth");
+      
+    });
+  }
 
-    // componentDidUpdate() {
-    //   const { signedIn } = this.props;
-    //   setTimeout(() => {
-    //     this.props.navigation.navigate(signedIn ? "App" : "Auth");
-    //   }, 2000);
-    // }
+  
 
-    componentDidMount() {
-        persistStore(store, null, () => {
-            const {signedIn} = this.props;
-            this.props.navigation.navigate(signedIn ? "App" : "Auth");
-            // setTimeout(() => {
-            //   this.props.navigation.navigate(signedIn ? "App" : "Auth");
-            // }, 1000);
-        })
-    }
-
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    flexDirection: "column",
-                    backgroundColor: "#fff"
-                }}
-            >
-                <View
-                    style={{
-                        width: "100%",
-                        height: "45%",
-                        alignContent: "flex-end",
-                        justifyContent: "flex-end"
-                    }}
-                >
-                    <Image
-                        source={require("./pages/assets/images/OyespaceRebrandingLogo.png")}
-                        style={{width: 150, height: 130, alignSelf: "center"}}
-                    />
-                </View>
-                <ActivityIndicator/>
-                <Text style={{fontSize: 8, color: "black", alignSelf: "center"}}>
-                    {" "}
-                    Data is loading now..
-                </Text>
-
-                {/*         <Text style={mystyles.splashHeadline}> OYE SAFE</Text>*/}
-                <Text style={mystyles.yourSafetyIsPriceless}>
-                    {" "}
-                    Your Safety is Priceless
-                </Text>
+  render() {
+    return (
+      <Fragment>
+        <SafeAreaView style={{ flex: 0, backgroundColor: "#ff8c00" }} />
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+          <View style={styles.container}>
+            <View style={{ flex: 1, flexDirection: "column" }}>
+              <View style={styles.ImageContainer}>
                 <Image
-                    source={require("./pages/assets/images/building_complex.png")}
-                    style={{width: "100%", height: "35%", alignSelf: "center"}}
+                  source={require("./icons/headerLogo.png")}
+                  style={styles.topImageLogo}
                 />
+              </View>
+              <View style={styles.activityIndicatorContainer}>
+                <ActivityIndicator size="large" color="orange" />
+                {/* <Spinner
+                  isVisible={true}
+                  color={"orange"}
+                  size={50}
+                  type={"Circle"}
+                /> */}
+              </View>
+              <View style={styles.textContainer}>
+                <Text style={styles.text}>Your safety is priceless</Text>
+              </View>
+              <View style={styles.footerImageContainer}>
+                <Image
+                  source={require("./icons/img4.jpg")}
+                  style={styles.bottomImage}
+                />
+              </View>
             </View>
-        );
-    }
+          </View>
+        </SafeAreaView>
+      </Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        signedIn: state.UserReducer.signedIn,
-        OyespaceReducer: state.OyespaceReducer,
-        UserReducer: state.UserReducer
-    };
+  return {
+    signedIn: state.UserReducer.signedIn,
+    OyespaceReducer: state.OyespaceReducer,
+    UserReducer: state.UserReducer
+  };
 };
 
 const LoadingConnect = connect(mapStateToProps)(Loading);
 
 const App = createAppContainer(
-    createSwitchNavigator(
-        {
-            AuthLoading: LoadingConnect,
-            App: DashStack,
-            Auth: AuthStack
-        },
-        {
-            initialRouteName: "AuthLoading"
-        }
-    )
+  createSwitchNavigator(
+    {
+      AuthLoading: LoadingConnect,
+      App: DashStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "AuthLoading"
+    }
+  )
 );
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff"
+  },
+  containers: {},
+  ImageContainer: {
+    //   backgroundColor:'yellow',
+    width: Dimensions.get("screen").width,
+    height: hp("30%"),
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  topImageLogo: {
+    height: hp("20%"),
+    width: wp("60%")
+    // backgroundColor:'yellow'
+  },
+  activityIndicatorContainer: {
+    width: Dimensions.get("screen").width,
+    height: hp("15%"),
+    //   backgroundColor:'red',
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  textContainer: {
+    // backgroundColor:'blue',
+    height: hp("15%"),
+    width: Dimensions.get("screen").width,
+    alignItems: "center",
+    justifyContent: "flex-start"
+  },
+  text: {
+    fontWeight: "500",
+    fontSize: hp("3%"),
+    color:'#000',
+    
+  },
+  footerImageContainer: {
+    // backgroundColor:'green',
+    alignItems: "center",
+    justifyContent: "center",
+    height: hp("40%"),
+    width: Dimensions.get("screen").width
+  },
+  bottomImage: {
+    width: Dimensions.get("window").width,
+    height: wp("80%")
+  }
+});
