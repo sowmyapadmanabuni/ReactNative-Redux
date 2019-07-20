cbfb8c51ecb012b5570577d52ca48ab21142b43b
traxx10 <chuksben16@gmail.com>
2019-07-20 08:08:18 -0500

    Added notficaton codes

--- src/actions/NotificationAction.js
+++ src/actions/NotificationAction.js
@@ -63,10 +63,12 @@ export const getNotifications = (oyeURL, MyAccountID) => {
         const uniqueJoin = _.uniqBy(joinNotif, "sbSubID");
         let allNotifs = [...gateAppNotif, ...uniqueJoinStat, ...uniqueJoin];
 
-        const sorted = _.sortBy(allNotifs, [
-          "ntdCreated",
-          "ntdUpdated"
-        ]).reverse();
+        // const sorted = _.sortBy(allNotifs, [
+        //   "ntdCreated",
+        //   "ntdUpdated"
+        // ]).reverse();
+
+        const sorted = _.sortBy(allNotifs, ["ntdUpdated"]).reverse();
 
         sorted.map(data => {
           console.log(data.ntIsActive);
@@ -503,10 +505,11 @@ export const refreshNotifications = (oyeURL, MyAccountID) => {
         const uniqueJoin = _.uniqBy(joinNotif, "sbSubID");
         let allNotifs = [...gateAppNotif, ...uniqueJoinStat, ...uniqueJoin];
 
-        const sorted = _.sortBy(allNotifs, [
-          "ntdCreated",
-          "ntdUpdated"
-        ]).reverse();
+        // const sorted = _.sortBy(allNotifs, [
+        //   "ntdCreated",
+        //   "ntdUpdated"
+        // ]).reverse();
+        const sorted = _.sortBy(allNotifs, ["ntdUpdated"]).reverse();
 
         dispatch({
           type: REFRESH_NOTIFICATION_SUCCESS,
