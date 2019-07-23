2435333981e0ceca77724fb119ada7e68b787f65
Jyothi Menda <jyothi.menda@synclovis.com>
2019-07-23 13:20:47 +0530

    Resolving BugFixes 23rd July

--- src/screens/Resident/Dashboard/Dashboard.js
+++ src/screens/Resident/Dashboard/Dashboard.js
@@ -1158,8 +1158,8 @@ class Dashboard extends React.Component {
                         marginTop={20}
                         backgroundColor={base.theme.colors.cardBackground}
                         onCardClick={() =>
-                           // this.props.navigation.navigate("MyVehicleListScreen")
-                            this.props.navigation.navigate("subscriptionManagement")
+                           this.props.navigation.navigate("MyVehicleListScreen")
+                            //this.props.navigation.navigate("subscriptionManagement")
 
                         }
                     />
