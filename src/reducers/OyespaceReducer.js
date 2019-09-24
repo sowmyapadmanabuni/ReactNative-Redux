da9c239a81184742dc9c254a95075447944ac2c3
traxx10 <chuksben16@gmail.com>
2019-09-24 01:26:04 -0500

    about to pull

--- src/reducers/OyespaceReducer.js
+++ src/reducers/OyespaceReducer.js
@@ -5,7 +5,7 @@
     2. apidev  --- For develpment server
     3. api     --- For production server
 */
-let urlType = 'apiuat';
+let urlType = 'api';
 
 const oyeURL = `${urlType}.oyespace.com`; //Validation/
 
@@ -16,26 +16,26 @@ const mediaUpload = 'https://mediauploaduat.oyespace.com/Images/'; //Validation
 const oyeBaseURL = 'http://' + oyeURL + '/oye247/api/v1/';
 
 const INITIAL_STATE = {
-    oyeURL,
-    champBaseURL: 'http://' + oyeURL + '/oyeliving/api/v1/',
-    oye247BaseURL: 'https://' + oyeURL + '/oye247/api/v1/',
+  oyeURL,
+  champBaseURL: 'http://' + oyeURL + '/oyeliving/api/v1/',
+  oye247BaseURL: 'https://' + oyeURL + '/oye247/api/v1/',
 
-    oyeBaseURL,
-    viewImageURL: 'http://' + oyeBaseURL + '/Images/',
-    uploadImageURL:
-        'http://' + oyeBaseURL + '/oyeliving/OyeLivingApi/v1/association/upload',
-    oyeMobileRegex: /^[0]?[456789]d{9}$/,
-    oyeNonSpecialRegex: /[^0-9A-Za-z ,]/,
-    oyeNonSpecialNameRegex: /[^0-9A-Za-z .]/,
-    oyeEmailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    OyeFullName: /^[a-zA-Z ]+$/,
-    mediaupload: mediaUpload
-    // noImage: noImage
+  oyeBaseURL,
+  viewImageURL: 'http://' + oyeBaseURL + '/Images/',
+  uploadImageURL:
+    'http://' + oyeBaseURL + '/oyeliving/OyeLivingApi/v1/association/upload',
+  oyeMobileRegex: /^[0]?[456789]d{9}$/,
+  oyeNonSpecialRegex: /[^0-9A-Za-z ,]/,
+  oyeNonSpecialNameRegex: /[^0-9A-Za-z .]/,
+  oyeEmailRegex: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  OyeFullName: /^[a-zA-Z ]+$/,
+  mediaupload: mediaUpload
+  // noImage: noImage
 };
 
 export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
